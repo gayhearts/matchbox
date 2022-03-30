594d98b326822bbafdb66cd33dee0b2ca4f00389
Sunroses <deli73@users.noreply.github.com>
2022-03-30 00:01:17 -0700

    usual moddev setup stuff

---
+++ src/main/java/xyz/sunrose/matchbox/Matchbox.java
@@ -0,0 +1,14 @@
+package xyz.sunrose.matchbox;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Matchbox implements ModInitializer {
+	public static final String MODID = "matchbox";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
