164406bb159ea759221cc34f2c6068e246e0ce10
Sunroses <deli73@users.noreply.github.com>
2022-04-04 18:11:09 -0700

    update/add connection side calculation

--- src/main/java/xyz/sunrose/matchbox/items/DetacherTool.java
+++ src/main/java/xyz/sunrose/matchbox/items/DetacherTool.java
@@ -9,12 +9,14 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
+import xyz.sunrose.matchbox.Matchbox;
 
 public class DetacherTool extends Item {
     public DetacherTool(Settings settings) {
@@ -27,36 +29,20 @@ public class DetacherTool extends Item {
         World world = context.getWorld();
         BlockPos blockPos = context.getBlockPos();
         BlockState blockState = world.getBlockState(blockPos);
-        if(blockState.getBlock() instanceof HorizontalConnectingBlock block) {
-            if (  blockState.get(ConnectingBlock.NORTH)
-                ||blockState.get(ConnectingBlock.EAST)
-                ||blockState.get(ConnectingBlock.SOUTH)
-                ||blockState.get(ConnectingBlock.WEST)) {
-
-                //this is a mess lol but basically just detach all the sides
-                //this would be so much easier in haskell smh
-                BlockState finalState = detachSide(
-                        world, detachSide(
-                                world, detachSide(
-                                        world, detachSide(
-                                                world, blockState, blockPos, Direction.NORTH
-                                        ), blockPos, Direction.EAST
-                                ), blockPos, Direction.SOUTH
-                        ), blockPos, Direction.WEST
-                );
-                world.setBlockState(blockPos, finalState, Block.NOTIFY_LISTENERS | Block.FORCE_STATE); //flags to not update?
+        if(blockState.getBlock() instanceof HorizontalConnectingBlock block) { // TODO maybe check for side properties instead of this
+            Direction side = hitSide(context);
+            if (  blockState.get(ConnectingBlock.FACING_PROPERTIES.get(side)) ) { // only execute if the relevant side has a connection
+                //detach the relevant side
+                BlockState finalState = detachSide(world, blockState, blockPos, side);
+                world.setBlockState(blockPos, finalState, Block.NOTIFY_LISTENERS | Block.FORCE_STATE); //flags to not update neighbors
                 // TODO custom sound
                 BlockSoundGroup soundGroup = block.getSoundGroup(blockState);
                 world.playSound(
                         player, blockPos, soundGroup.getBreakSound(),
                         SoundCategory.BLOCKS, 0.9f, soundGroup.getPitch() * 0.8f
                 );
+
                 world.emitGameEvent(player, GameEvent.BLOCK_CHANGE, blockPos);
-                if (player != null) {
-                    context.getStack().damage(1, player, (p) -> {
-                        p.sendToolBreakStatus(context.getHand());
-                    });
-                }
                 return ActionResult.success(world.isClient());
             }
         }
@@ -74,5 +60,32 @@ public class DetacherTool extends Item {
             world.setBlockState(pos.offset(dir), neighbor.with(ConnectingBlock.FACING_PROPERTIES.get(opposite), false), Block.NOTIFY_LISTENERS | Block.FORCE_STATE);
         }
         return newState;
+    }
+
+    private Direction hitSide(ItemUsageContext context) {
+        Vec3d pos = context.getHitPos();
+        //position relative to center of block
+        double relX = MathHelper.fractionalPart(pos.x) - 0.5;
+        double relZ = MathHelper.fractionalPart(pos.z) - 0.5;
+        double absX = MathHelper.sign(relX) * relX;
+        double absZ = MathHelper.sign(relZ) * relZ;
+
+        if (relX < 0 && absX >= absZ) { //negative X - West
+            return Direction.WEST;
+        }
+        else if (relX >= 0 && absX >= absZ) { //positive X - East
+            return Direction.EAST;
+        }
+        else if (relZ < 0 && absZ > absX) { //negative Z - North
+            return Direction.NORTH;
+        }
+        else if (relZ >= 0 && absZ > absX) { //positive Z - South
+            return Direction.SOUTH;
+        }
+        else { // ????
+            Matchbox.LOGGER.debug("Invalid hit location, defaulting to north");
+            return Direction.NORTH;
+        }
+
     }
 }
